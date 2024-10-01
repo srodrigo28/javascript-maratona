4224fc4bf450fd349f67ddbb9d62078b4a083ca7
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2024-10-01 08:23:48 -0300

    Create reduce-4.js

---
+++ reduce-4.js
@@ -0,0 +1,27 @@
+let alunos = [
+    {id: 1, nome: "Marcela Duarte Lemos", price: 300.00, cadastro: "30-09-2024" },
+    {id: 2, nome: "Manuela Duarte Lemos", price: 300.00, cadastro: "30-09-2024" },
+    {id: 3, nome: "Emerson Sabino",       price: 300.00, cadastro: "30-09-2024" },
+    {id: 4, nome: "Nathan Sabino",        price: 300.00, cadastro: "30-09-2024" },
+    {id: 5, nome: "Lucas Sousa",          price: 500.00, cadastro: "30-09-2024" },
+    {id: 6, nome: "Giovanni Miranda",     price: 1000.00, cadastro: "30-09-2024" },
+    {id: 7, nome: "Luiz Fernando",        price: 400.00, cadastro: "30-09-2024" },
+    {id: 8, nome: "Kauã",                 price: 400.00, cadastro: "30-09-2024" }
+]
+
+function listaDeAlunos(){
+    alunos.forEach(item => {
+        console.log(item.nome, item.price, item.cadastro)
+    })
+    console.log('\n')
+}
+//listaDeAlunos()
+
+function calcularFaturamento(){
+    const total = alunos.reduce((total, item) => {
+        return total + item.price
+    }, 0)
+    
+    console.log('Total faturamento: R$ ' + total)
+}
+//calcularFaturamento()
