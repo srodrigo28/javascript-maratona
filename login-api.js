d8ff34fadee9d9cef4a40962cd633373ad411214
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2024-10-03 15:05:04 -0300

    Create login-api.js

---
+++ login-api.js
@@ -0,0 +1,28 @@
+const url = "http://localhost:3000/users"
+
+const userName = "USER1 "
+const userPassword = "123 "
+
+function validar() {
+    userValido = userName.toLowerCase().trim()
+    PasswordValido = userPassword.toLowerCase().trim()
+
+    fetch(url,{
+        method: 'GET',
+        headers: { "Content-type": "application/json" }
+    })
+    .then( response => response.json() )
+    .then( users => {
+        const user = users.find( 
+            item => item.username == userValido
+            &&
+            item.password == PasswordValido
+        )
+        if(user){
+            console.log('Login bem-sucedido!')
+        } else {
+            console.log('Usuário ou senha incorretos');
+        }
+    })
+}
+validar()
