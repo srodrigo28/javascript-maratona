0766daf5b814f540ed886511efcbe5a8dea94c19
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2024-10-10 18:37:59 -0300

    Create funcoes-helpers.js

---
+++ funcoes-helpers.js
@@ -0,0 +1,18 @@
+function moeda(valor){
+    return valor.toLocaleString('pt-br', {
+        style: 'currency',
+        currency: 'BRL'
+    });
+}
+//console.log(moeda(100.99));
+
+// reduzir textos
+function stringText(text, size) {
+    return text.substring(0, size) + '...';
+}
+
+// encontra e substitui
+function stringImg(imgFomart){
+    return imgFomart.replace(/\w\.jpg/gi, 'W.jpg');
+}
+// console.log(stringImg("img.jpg"))
