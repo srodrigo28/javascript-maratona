194d9f34b3dde7d379964d30cf04a397484c355d
SEBASTIAO RODRIGO NASCIMENTO SOUSA <rodrigoexer1@gmail.com>
2025-03-25 21:03:32 -0300

    Update mpa2.js

--- map/mpa2.js
+++ map/mpa2.js
@@ -1,1 +1,16 @@
 
+const users = data.map((user, index) => {
+  if(user.salary <= 2500) {
+    user.salary += 900;
+    user.bonus +=1;
+  }
+
+  return{
+    name: user.name,
+    salary: user.salary,
+    bonus: user.bonus
+  }
+})
+
+console.log(users);
+console.log(data);
